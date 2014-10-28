20791a6c8327e05adfd5f3c811b16352fc176b3d
kaiyiz <kaiyiz@codeaurora.org>
2014-10-28 13:25:55 +0800

    MMS: Fix The slideshow can't be shown in message
    After the MessageItem Pdu Loaded, the MessageItem's AttachmentType
    might have changed. So precisely, it's not the same MessageItem
    anymore.Since it's not the same MessageItem, we should setImage,
    and the slideshow will be shown normally.
    
    After Pdu Loaded, when bindCommonMessage again, the sameItem
    argument should be false, because it's not the same MessageItem
    anymore.
    
    CRs-Fixed: 741025
    
    Change-Id: Ibef5ca408d86ba76e730fe1fc4117d953d6c74fa

--- src/com/android/mms/ui/MessageListItem.java
+++ src/com/android/mms/ui/MessageListItem.java
@@ -474,6 +474,7 @@ public class MessageListItem extends LinearLayout implements
                 showMmsView(false);
             }
             if (mMessageItem.mSlideshow == null) {
+                final int mCurrentAttachmentType = mMessageItem.mAttachmentType;
                 mMessageItem.setOnPduLoaded(new MessageItem.PduLoadedCallback() {
                     public void onPduLoaded(MessageItem messageItem) {
                         if (DEBUG) {
@@ -485,7 +486,8 @@ public class MessageListItem extends LinearLayout implements
                         if (messageItem != null && mMessageItem != null &&
                                 messageItem.getMessageId() == mMessageItem.getMessageId()) {
                             mMessageItem.setCachedFormattedMessage(null);
-                            bindCommonMessage(true);
+                            bindCommonMessage(
+                                    mCurrentAttachmentType == messageItem.mAttachmentType);
                         }
                     }
                 });
