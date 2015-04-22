CyanogenMod/android_packages_apps_Mms-caf

Mms: add a drawable for the calendar event
Change-Id: I367e44f98b217afe4ae5eb9336e6fcf0358d61cf

src/com/android/mms/ui/AttachmentTypeSelectorAdapter.java
@@ -98,7 +98,7 @@ public class AttachmentTypeSelectorAdapter extends IconListAdapter {
 
         // calendar event support
         addItem(data, context.getResources().getString(R.string.attach_add_calendar_events),
-                R.drawable.ic_attach_vcard, ADD_CALENDAR_EVENTS);
+                R.drawable.ic_attach_invitation, ADD_CALENDAR_EVENTS);
 
         return data;
     }

FusionSP/android_packages_apps_Mms

(cherry picked from commit 3c3806adc3bad904d07b87d0b597ad16bdf142d9)

src/com/android/mms/ui/AttachmentTypeSelectorAdapter.java
@@ -49,7 +49,7 @@ public class AttachmentTypeSelectorAdapter extends IconListAdapter {
     public AttachmentTypeSelectorAdapter(Context context, int mode) {
         super(context, getData(mode, context));
     }
-    
+
     public int buttonToCommand(int whichButton) {
         AttachmentListItem item = (AttachmentListItem)getItem(whichButton);
         return item.getCommand();
@@ -98,7 +98,7 @@ public class AttachmentTypeSelectorAdapter extends IconListAdapter {
 
         // calendar event support
         addItem(data, context.getResources().getString(R.string.attach_add_calendar_events),
-                R.drawable.ic_attach_vcard, ADD_CALENDAR_EVENTS);
+                R.drawable.ic_attach_event, ADD_CALENDAR_EVENTS);
 
         return data;
     }
@@ -117,7 +117,7 @@ public class AttachmentTypeSelectorAdapter extends IconListAdapter {
         AttachmentListItem temp = new AttachmentListItem(title, resource, command);
         data.add(temp);
     }
-    
+
     public static class AttachmentListItem extends IconListAdapter.IconListItem {
         private int mCommand;
 